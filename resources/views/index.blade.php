28afd0cf22ef2064c5d41909065a01541e9eca6e
marencoalf <alfredomarenco@boletea.com>
2020-11-20 21:32:30 -0600

    Instalacion del Proyecto

---
+++ resources/views/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.template')
+
+@section('section')
+    <section class="bg-info" id="azul">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+    </section>
+    <section id="blanco">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+    </section>
+@endsection
