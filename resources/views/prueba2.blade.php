0828a2f652fa30b394af98145a905332db7df666
marencoalf <alfredomarenco@boletea.com>
2020-11-25 21:12:33 -0600

    Nuevo commit en la rama frontend

--- resources/views/prueba2.blade.php
+++ resources/views/prueba2.blade.php
@@ -2,3 +2,5 @@
 @section('section2')
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, labore distinctio temporibus minima odit necessitatibus reiciendis voluptate architecto ipsa eveniet laborum optio autem vero quisquam consequuntur veritatis quia ratione quidem!
 @endsection
+
+este es una prueba
