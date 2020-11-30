fa2580a4b22e99260355c664775f5b1e5ab136b7
alvarbu <74802594+alvarbu@users.noreply.github.com>
2020-11-30 14:40:55 -0600

    Cambios - Index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -88,25 +88,54 @@
 
 <section id="rosa">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 190"><path fill="#ee4a7b" fill-opacity="1" d="M0,160L60,149.3C120,139,240,117,360,122.7C480,128,600,160,720,170.7C840,181,960,171,1080,160C1200,149,1320,139,1380,133.3L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
-    <div class="row texto">
-        <div class="col-lg-3 col-md-3 col-sm-12">
-            <img src="/img/puntos.png" class="img-fluid" alt="">
-        </div>
-        <div class="col-lg-6 col-md-6 col-sm-12 centro">
-            <img src="/img/Icons/Calzada.svg" class="img-fluid calzada-rosa" alt="Gran Calzada | Ciudad Viva">
-            <h2 class="estas-en">Estás en el camino correcto para 
-                cambiar y mejorar tu estilo de vida, <br> 
-                es el momento de vivir otro entorno.</h2>
-                <a href="#" class="boton-rosa"><button class="btn btn-blue">Conoce los desarrollos</button></a>
+    <div class="fondo-1">
+        <div class="container-fluid">
+            <div class="row texto">
+                <div class="col-lg-12 col-md-12 col-sm-12 centro">
+                    <img src="/img/Icons/Calzada.svg" class="img-fluid calzada-rosa" alt="Gran Calzada | Ciudad Viva">
+                    <h2 class="estas-en">Estás en el camino correcto <br> para 
+                        cambiar y mejorar tu estilo de vida,<br> 
+                        es el momento de vivir otro entorno.</h2>
+                        <a href="#" class="boton-rosa"><button class="btn btn-blue">Conoce los desarrollos</button></a>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-3 col-md-3 col-sm-12 linea">
-                <img src="/img/linea.png" class="img-fluid " alt="">
-            </div>
-        </div>
-        <svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 250"><path fill="#ee4a7b" fill-opacity="1" d="M0,64L80,69.3C160,75,320,85,480,122.7C640,160,800,224,960,240C1120,256,1280,224,1360,208L1440,192L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
-        
+    </div>
     
+    <svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 250">  <path fill="#ee4a7b" fill-opacity="1" d="M0,64L80,69.3C160,75,320,85,480,122.7C640,160,800,224,960,240C1120,256,1280,224,1360,208L1440,192L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+    
+    
+</section> 
+
+<section id="casitas">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-12 col-sm-12">
+            <h2 class="respaldados">Respaldados por un <br>
+            Gran Desarrollador</h2>
+            </div>
+            <div class="col-lg-6 col-md-12 col-sm-12 casitas-col">
+                <img src="img/Icons/Logo-Casitas.svg" class="img-fluid casitas-l" alt="">
+            </div>
+        </div>
+        <div class="col-lg-12">
+            <p class="grupo-casitas">Grupo Casitas Desarrollos®, empresa con 15 años de experiencia diseñando, planeando, contruyendo y comercializando comunidades, en busca siempre de la innovación y sustentabilidad para otogar a los habitantes de sus desarrollos beneficios excluivos de calidad, ubicación y diseño.</p>
+        </div>
+
+       <div class="row">
+           <div class="col-lg-12 comunidades">
+            <img src="img/creando-comunidades.png" alt="" class="img-fluid ">
+           </div>
+       </div>
+    </div>
+
+</section>
+<section>
     
 </section>
+
+
+
+
 
 @endsection
