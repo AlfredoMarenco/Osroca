1b43484c3bc2afec1c019d07efaa5ee194ff39a4
marencoalf <alfredomarenco@boletea.com>
2020-12-03 22:50:24 -0600

    Agregando funciones genericas

--- resources/views/violeta-plus.blade.php
+++ resources/views/violeta-plus.blade.php
@@ -5,10 +5,10 @@
    <div class="container-fluid">
     <div class="row">
         <div class="col-lg-5 col-md-12 col-sm-12">
-            <img src="/img/Icons/Logo-Almada-inv.png" class="img-fluid logo-h" alt="">
+            <img src="{{ asset('img/Icons/Logo-Almada-inv.png') }}" class="img-fluid logo-h" alt="">
         </div>
         <div class="col-lg-7 col-md-12 col-sm-12 p-0">
-            <img src="/{{ asset('img/violeta-plus/cabeceras-fachada-1.jpg') }}" class="img-fluid fachada-violeta" alt="">
+            <img src="{{ asset('img/violeta-plus/cabeceras-fachada-1.jpg') }}" class="img-fluid fachada-violeta" alt="">
         </div>
     </div>
    </div>
@@ -20,7 +20,7 @@
     <div class="container-fluid">
         <div class="row ameni">
             <div class="col-lg-4 col-md-4 col-sm-6 imag-planta">
-                <img src="{{ asset('/img/violeta-plus/GC_Violeta_Plus_Planta_Baja_02.png') }}" class="img-fluid planta-b" alt="">
+                <img src="{{ asset('img/violeta-plus/GC_Violeta_Plus_Planta_Baja_02.png') }}" class="img-fluid planta-b" alt="">
             </div>
             <div class="col-lg-4 col-md-4 col-sm-6 ameni-txt">
                 <h1>Modelo Violeta Plus</h1>
@@ -50,7 +50,7 @@
                 <br>
             </div>
             <div class="col-lg-4 col-md-4 col-sm-6 imag-planta">
-                <img src="{{ asset('/img/violeta-plus/GC_Violeta_Plus_Planta_Alta02.png') }}" class="img-fluid planta-b" alt="">
+                <img src="{{ asset('img/violeta-plus/GC_Violeta_Plus_Planta_Alta02.png') }}" class="img-fluid planta-b" alt="">
             </div>
 
         </div>
@@ -68,7 +68,7 @@
 
                 <h2 class="un-desarrollo" style="text-align: center;">Un desarrollo de</h2>
                 <div style="text-align: center">
-                    <img src="{{ asset('/img/Icons/Logo-Calzada.svg') }}" alt="" class="img-fluid circulo-calzada">
+                    <img src="{{ asset('img/Icons/Logo-Calzada.svg') }}" alt="" class="img-fluid circulo-calzada">
                 </div>
            </div>
            <div class="col-lg-6 col-md-12 col-sm-12 formulario-2">
