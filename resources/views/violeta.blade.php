e78589f1d50702585144ed908eac746126f32511
marencoalf <alfredomarenco@boletea.com>
2020-12-03 22:48:54 -0600

    Agregando funciones genericas

--- resources/views/violeta.blade.php
+++ resources/views/violeta.blade.php
@@ -5,11 +5,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-5 col-md-12 col-sm-12">
-                <img src="/img/Icons/Logo-Almada-inv.png" class="img-fluid logo-h" alt="">
+                <img src="{{ asset('/img/Icons/Logo-Almada-inv.png') }}" class="img-fluid logo-h" alt="">
             </div>
             <div class="col-lg-7 col-md-12 col-sm-12 p-0">
-                <img src="/img/violeta/GC_Violeta_Fachada-1.jpg" class="img-fluid fachada-violeta" alt="">
-            </div>   
+                <img src="{{ asset('/img/violeta/GC_Violeta_Fachada-1.jpg') }}" class="img-fluid fachada-violeta" alt="">
+            </div>
     </div>
 </section>
 
@@ -18,7 +18,7 @@
     <div class="container-fluid">
         <div class="row ameni">
             <div class="col-lg-4 col-md-4 col-sm-6 imag-planta">
-                <img src="/img/violeta/GC_Violeta_Planta_Baja.png" class="img-fluid planta-b" alt="">
+                <img src="{{ asset('/img/violeta/GC_Violeta_Planta_Baja.png') }}" class="img-fluid planta-b" alt="">
             </div>
             <div class="col-lg-4 col-md-4 col-sm-6  violeta">
                 <h1>Modelo Violeta</h1>
@@ -27,7 +27,7 @@
                 <br>
                 <li><b>Casa de 2 plantas <br>
                     Amplio patio <br>
-                    Lista para ampliación</b> 
+                    Lista para ampliación</b>
                 </li> <br>
                 <li style="font-weight: 600" class="boold">Planta Arquitectónica</li> <br>
                 <li style="font-weight: 600" class="boold">PLANTA BAJA</li>
@@ -40,16 +40,16 @@
                 <li>Recámara secundaria con área
                     para clóset</li>
                 <li>1 Baño Completo</li>
-                
-               
-                
+
+
+
 
                 <br>
             </div>
             <div class="col-lg-4 col-md-4 col-sm-6 imag-planta">
-                <img src="/img/violeta/GC_Violeta_Planta_Alta2.png" class="img-fluid planta-b" alt="">
+                <img src="{{ asset('/img/violeta/GC_Violeta_Planta_Alta2.png') }}" class="img-fluid planta-b" alt="">
             </div>
-          
+
         </div>
     </div>
 
@@ -62,34 +62,34 @@
    <div class="container-fluid">
        <div class="row">
            <div class="col-lg-6 col-md-12 col-sm-12">
-            
+
                 <h2 class="un-desarrollo" style="text-align: center;">Un desarrollo de</h2>
                 <div style="text-align: center">
-                    <img src="/img/Icons/Logo-Calzada.svg" alt="" class="img-fluid circulo-calzada">
+                    <img src="{{ asset('/img/Icons/Logo-Calzada.svg') }}" alt="" class="img-fluid circulo-calzada">
                 </div>
            </div>
            <div class="col-lg-6 col-md-12 col-sm-12 formulario-2">
             <form class="contact" id="cotizacion" name="contact-form" method="post" action="enviar.php" >
                 <div class="form-group">
                     <h1 class="recibe-una" style="text-align: left">Recibe una cotización</h1>
-    
+
                   <input type="name" name="nombre" class="form-control" id="exampleFormControlInput1" required="required" placeholder="Nombre">
                 </div>
 
                 <div class="form-group">
-                  
+
                     <input type="tel" name="telefono" class="form-control" id="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required="required" placeholder="Teléfono">
                   </div>
 
                 <div class="form-group">
-                  
+
                   <input type="email" name="email" class="form-control" id="exampleFormControlInput1" required="required" placeholder="Correo">
                 </div>
 
                 <div class="form-group" style="text-align: right">
-      
-                    
-                
+
+
+
                 <button type="submit" class="btn btn-pink">Enviar</button>
               </form>
         </div>
