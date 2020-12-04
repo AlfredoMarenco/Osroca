5fa360910eeec9ba11c7e58b661615f3a021cdfb
marencoalf <alfredomarenco@boletea.com>
2020-12-03 22:47:40 -0600

    Agregando funciones genericas

--- resources/views/violeta-plus.blade.php
+++ resources/views/violeta-plus.blade.php
@@ -8,11 +8,11 @@
             <img src="/img/Icons/Logo-Almada-inv.png" class="img-fluid logo-h" alt="">
         </div>
         <div class="col-lg-7 col-md-12 col-sm-12 p-0">
-            <img src="/img/violeta-plus/cabeceras-fachada-1.jpg" class="img-fluid fachada-violeta" alt="">
+            <img src="/{{ asset('img/violeta-plus/cabeceras-fachada-1.jpg') }}" class="img-fluid fachada-violeta" alt="">
         </div>
     </div>
    </div>
-   
+
 </section>
 
 
@@ -20,7 +20,7 @@
     <div class="container-fluid">
         <div class="row ameni">
             <div class="col-lg-4 col-md-4 col-sm-6 imag-planta">
-                <img src="/img/violeta-plus/GC_Violeta_Plus_Planta_Baja_02.png" class="img-fluid planta-b" alt="">
+                <img src="{{ asset('/img/violeta-plus/GC_Violeta_Plus_Planta_Baja_02.png') }}" class="img-fluid planta-b" alt="">
             </div>
             <div class="col-lg-4 col-md-4 col-sm-6 ameni-txt">
                 <h1>Modelo Violeta Plus</h1>
@@ -28,7 +28,7 @@
                 <li>Construcción: 65.73 m2</li>
                 <br>
 
-                <li>Casa de 2 plantas <br> 
+                <li>Casa de 2 plantas <br>
                 Pasillo lateral <br>
                 Amplio patio <br>
                 </li> <br>
@@ -43,16 +43,16 @@
                 <li>Recámara secundaria con área
                     para clóset</li>
                 <li>1 Baño Completo</li>
-                
-               
-                
+
+
+
 
                 <br>
             </div>
             <div class="col-lg-4 col-md-4 col-sm-6 imag-planta">
-                <img src="/img/violeta-plus/GC_Violeta_Plus_Planta_Alta02.png" class="img-fluid planta-b" alt="">
+                <img src="{{ asset('/img/violeta-plus/GC_Violeta_Plus_Planta_Alta02.png') }}" class="img-fluid planta-b" alt="">
             </div>
-          
+
         </div>
     </div>
 
@@ -65,34 +65,34 @@
    <div class="container-fluid">
        <div class="row">
            <div class="col-lg-6 col-md-12 col-sm-12">
-            
+
                 <h2 class="un-desarrollo" style="text-align: center;">Un desarrollo de</h2>
                 <div style="text-align: center">
-                    <img src="/img/Icons/Logo-Calzada.svg" alt="" class="img-fluid circulo-calzada">
+                    <img src="{{ asset('/img/Icons/Logo-Calzada.svg') }}" alt="" class="img-fluid circulo-calzada">
                 </div>
            </div>
            <div class="col-lg-6 col-md-12 col-sm-12 formulario-2">
             <form class="contact" id="cotizacion" name="contact-form" method="post" action="enviar.php" >
                 <div class="form-group">
                     <h1 class="recibe-una" style="text-align: left">Recibe una cotización</h1>
-    
+
                   <input type="name" name="nombre" class="form-control" id="exampleFormControlInput1" required="required" placeholder="Nombre">
                 </div>
 
                 <div class="form-group">
-                  
+
                     <input type="tel" name="telefono" class="form-control" id="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required="required" placeholder="Teléfono">
                   </div>
 
                 <div class="form-group">
-                  
+
                   <input type="email" name="email" class="form-control" id="exampleFormControlInput1" required="required" placeholder="Correo">
                 </div>
 
                 <div class="form-group" style="text-align: right">
-      
-                    
-                
+
+
+
                 <button type="submit" class="btn btn-pink">Enviar</button>
               </form>
         </div>
