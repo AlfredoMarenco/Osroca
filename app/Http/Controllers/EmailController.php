d83e61cd7aa398aaea53eb64714278a67b28241b
marencoalf <alfredomarenco@boletea.com>
2021-04-20 17:37:11 -0500

    Cambios en los formularios de ficha tecnica

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -11,7 +11,6 @@ class EmailController extends Controller
 {
     public function sendEmail(Request $request)
     {
-
         $modelo = $request->modelo;
         $nombre = $request->nombre;
         $correo = $request->correo;
@@ -22,35 +21,35 @@ class EmailController extends Controller
 
         switch ($modelo) {
             case 'balche':
-                Mail::to($correo)->locale('es')->send(new Leads('Miraverde - Balche', $nombre, $correo, $telefono, $presupuesto, $credito, $contacto));
+                Mail::to(['alvarbu@gmail.com','dev@agenciavandu.com'])->locale('es')->send(new Leads('Miraverde - Balche', $nombre, $correo, $telefono, $presupuesto, $credito, $contacto));
                 return redirect('/download/ficha-tecnica-balche.pdf');
                 break;
             case 'flamboyanplus':
-                Mail::to($correo)->locale('es')->send(new Leads('Miraverde - Flamboyan Plus', $nombre, $correo, $telefono, $presupuesto, $credito, $contacto));
+                Mail::to(['alvarbu@gmail.com','dev@agenciavandu.com'])->locale('es')->send(new Leads('Miraverde - Flamboyan Plus', $nombre, $correo, $telefono, $presupuesto, $credito, $contacto));
                 return redirect('/download/ficha-tecnica-flamboyan-plus.pdf');
                 break;
             case 'bugambiliaplus':
-                Mail::to($correo)->locale('es')->send(new Leads('Miraverde - Bugambilia Plus', $nombre, $correo, $telefono, $presupuesto, $credito, $contacto));
+                Mail::to(['alvarbu@gmail.com','dev@agenciavandu.com'])->locale('es')->send(new Leads('Miraverde - Bugambilia Plus', $nombre, $correo, $telefono, $presupuesto, $credito, $contacto));
                 return redirect('/download/ficha-tecnica-bugambilia-plus.pdf');
                 break;
             case 'ceibaplus':
-                Mail::to($correo)->locale('es')->send(new Leads('Miraverde - Ceiba Plus', $nombre, $correo, $telefono, $presupuesto, $credito, $contacto));
+                Mail::to(['alvarbu@gmail.com','dev@agenciavandu.com'])->locale('es')->send(new Leads('Miraverde - Ceiba Plus', $nombre, $correo, $telefono, $presupuesto, $credito, $contacto));
                 return redirect('/download/ficha-tecnica-ceiba-plus.pdf');
                 break;
             case 'aguamarina':
-                Mail::to($correo)->locale('es')->send(new Leads('Almada - Aguamarina', $nombre, $correo, $telefono, $presupuesto, $credito, $contacto));
+                Mail::to(['alvarbu@gmail.com','dev@agenciavandu.com'])->locale('es')->send(new Leads('Almada - Aguamarina', $nombre, $correo, $telefono, $presupuesto, $credito, $contacto));
                 return redirect('/download/Aguamarina.pdf');
                 break;
             case 'magenta':
-                Mail::to($correo)->locale('es')->send(new Leads('Almada - Magenta', $nombre, $correo, $telefono, $presupuesto, $credito, $contacto));
+                Mail::to(['alvarbu@gmail.com','dev@agenciavandu.com'])->locale('es')->send(new Leads('Almada - Magenta', $nombre, $correo, $telefono, $presupuesto, $credito, $contacto));
                 return redirect('/download/Magenta.pdf');
                 break;
             case 'violeta':
-                Mail::to($correo)->locale('es')->send(new Leads('Almada - Violeta', $nombre, $correo, $telefono, $presupuesto, $credito, $contacto));
+                Mail::to(['alvarbu@gmail.com','dev@agenciavandu.com'])->locale('es')->send(new Leads('Almada - Violeta', $nombre, $correo, $telefono, $presupuesto, $credito, $contacto));
                 return redirect('/download/Violeta.pdf');
                 break;
             case 'violetaplus':
-                Mail::to($correo)->locale('es')->send(new Leads('Almada - Violeta Plus', $nombre, $correo, $telefono, $presupuesto, $credito, $contacto));
+                Mail::to(['alvarbu@gmail.com','dev@agenciavandu.com'])->locale('es')->send(new Leads('Almada - Violeta Plus', $nombre, $correo, $telefono, $presupuesto, $credito, $contacto));
                 return redirect('/download/Cioleta-plus.pdf');
                 break;
         }
