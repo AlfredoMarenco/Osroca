bf589c5a4446b3e28cb14416fa034a552fa1c955
marencoalf <alfredomarenco@boletea.com>
2021-04-20 19:08:59 -0500

    Cambios en los formularios de ficha tecnica

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Gran Calzada')
-<img src="{{ asset('img/Icons/Calzada-1.png') }}" width="50%" alt="Laravel Logo">
+<img src="{{ asset('img/Icons/Calzada-1.png') }}" width="25%" alt="Laravel Logo">
 @else
 {{ $slot }}
 @endif
