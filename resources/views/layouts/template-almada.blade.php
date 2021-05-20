53fdb0d6f0f63a3ff0f3763b1e26421c1935c7f8
marencoalf <alfredomarenco@boletea.com>
2021-05-20 10:25:47 -0500

    Modificacion en codigo de seguimiento de google

--- resources/views/layouts/template-almada.blade.php
+++ resources/views/layouts/template-almada.blade.php
@@ -56,18 +56,23 @@
 
     </script>
     <noscript><img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=2821273661220502&ev=PageView&noscript=1" /></noscript>
+            src="https://www.facebook.com/tr?id=2821273661220502&ev=PageView&noscript=1">
+    </noscript>
     <!-- End Facebook Pixel Code -->
 
-<!-- Global site tag (gtag.js) - Google Ads: 785328543 -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-785328543"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    <!-- Global site tag (gtag.js) - Google Ads: 785328543 -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-785328543"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-  gtag('config', 'AW-785328543');
-</script>
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'AW-785328543');
+
+    </script>
 </head>
 
 <body>
@@ -228,7 +233,7 @@
     </script>
 
     @include('sweetalert::alert')
-    
+
     <!-- Start of HubSpot Embed Code -->
     <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/5510950.js"></script>
     <!-- End of HubSpot Embed Code -->
